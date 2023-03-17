5760b6b6c3735e4d6ea7d88564b04d73ee7a9e80
gabrielaliera <gliera2990@gmail.com>
2023-03-17 13:26:50 -0700

    Add coinInfo component to return

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import './App.css'
+import CoinInfo from "./Components/coinInfo";
 
 const API_KEY = import.meta.env.VITE_APP_API_KEY;
 
@@ -9,9 +10,7 @@ function App() {
   useEffect(() => {
 
     const fetchAllCoinData = async () => {
-      const response = await fetch(
-        "https://min-api.cryptocompare.com/data/all/coinlist?&api_key" + API_KEY
-      );
+      const response = await fetch("https://min-api.cryptocompare.com/data/all/coinlist?&api_key" + API_KEY);
       const json = await response.json();
       setList(json);
     };
@@ -21,14 +20,17 @@ function App() {
 
   }, []);
   
- console.log(list.Data)
   return (
     <div className="whole-page">
       <h1>My Crypto List</h1>
         <ul>
         {list && Object.entries(list.Data).map(([coin]) =>
           list.Data[coin].PlatformType === "blockchain" ? (
-            <li key={list.Data[coin].FullName}>{list.Data[coin].FullName}</li>
+            <CoinInfo
+              image={list.Data[coin].ImageUrl}
+              name={list.Data[coin].FullName}
+              symbol={list.Data[coin].Symbol}
+            />
           ) : null
         )}
           
