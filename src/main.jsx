a0eb22ff9f9ada08c67c9f3ed0883031c708df9d
gabrielaliera <gliera2990@gmail.com>
2023-03-24 12:59:35 -0700

    Update route

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Layout from './routes/Layout';
+import DetailView from './routes/DetailView';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+<BrowserRouter>
     <Routes>
-        <Route index={true} path="/" element={<App />} />
+      <Route path="/" element={<Layout />}>
+        <Route index={true} element={<App />} />
+        <Route index={false} path="/coinDetails/:symbol" element={<DetailView />} />
+      </Route>
     </Routes>
   </BrowserRouter>
 )
