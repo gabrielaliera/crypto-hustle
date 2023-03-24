a31098de111768414d31c3d00b665e1a34c092e5
gabrielaliera <gliera2990@gmail.com>
2023-03-24 12:36:21 -0700

    debug link component

--- src/routes/Layout.jsx
+++ src/routes/Layout.jsx
@@ -6,7 +6,7 @@ const Layout = () => {
       <nav>
         <ul>
           <li className="home-link" key="home-button">
-            <Link style=Unspecified to="/">
+            <Link to="/">
               Home
             </Link>
           </li>
