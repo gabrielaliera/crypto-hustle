f87f9bb79063195b8be9db1f77e735fc3bb0e988
gabrielaliera <gliera2990@gmail.com>
2023-03-27 14:02:56 -0700

    Add AbortController

--- src/Components/coinInfo.jsx
+++ src/Components/coinInfo.jsx
@@ -7,16 +7,33 @@ const API_KEY = import.meta.env.VITE_APP_API_KEY;
 
 const CoinInfo = ({ image, name, symbol, index}) => {
     const [price, setPrice] = useState(null);
-  
+    
+
     useEffect(() => {
+
+      const controller = new AbortController();
+
       const getCoinPrice = async () => {
-        const response = await fetch(
-          `https://min-api.cryptocompare.com/data/price?fsym=${symbol}&tsyms=USD&api_key=` + API_KEY
-        );
-        const json = await response.json();
-        setPrice(json);
+        try {
+          const response = await fetch(
+            `https://min-api.cryptocompare.com/data/price?fsym=${symbol}&tsyms=USD&api_key=` +
+              API_KEY,
+            { signal: controller.signal }
+          );
+          const json = await response.json();
+          setPrice(json);
+        } catch (error) {
+          if (error.name === "AbortError") {
+            // It's ok, don't do anything
+          } else {
+            console.error(error);
+          }
+        }
       };
+
       getCoinPrice().catch(console.error);
+      return () => controller.abort();
+
     }, [symbol]);
   
       return (
