42ec030243ed229fd537bc7e3109f5c957b3f8c4
gabrielaliera <gliera2990@gmail.com>
2023-03-27 13:37:36 -0700

    Add not found comp.

---
+++ src/routes/NotFound.jsx
@@ -0,0 +1,18 @@
+import { Outlet, Link } from "react-router-dom";
+
+const NotFound = () => {
+
+
+    return (
+        <div>
+            <h1>There;s noting here!</h1>
+            <Link to="/">
+              Back to Home
+            </Link>
+        </div>
+
+    );
+
+}
+
+export default NotFound
